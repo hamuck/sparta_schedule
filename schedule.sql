e3ac84b1aaf01885122972cba7aae9d951ffd901
박예진 <qkrdpwls2002>
2024-10-31 17:04:45 +0900

    fix : sql 파일 수정
    sql 파일의 where 부분을 scheduleId로 조건을 바꿈

--- schedule.sql
+++ schedule.sql
@@ -1,17 +1,17 @@
 CREATE TABLE Schedule(
     scheduleId integer not null primary key,
     userId integer not null foreign key,
-    schedulePassword varchar not null,
+    schedulePassword varchar(50) not null,
     createAt datetime not null,
     updateAt datetime not null,
-    title varchar not null,
+    title varchar(100) not null,
     content varchar(500)
 );
 
 CREATE TABLE User(
     userId integer not null primary key,
-    username varchar not null,
-    usermail varchar
+    username varchar(50) not null,
+    usermail varchar(50)
 );
 
 INSERT INTO Schedule(
@@ -27,7 +27,7 @@ SELECT * FROM Schedule;
 SELECT * FROM Schedule WHERE DATE(updateAt) = "2024-10-24";
 SELECT * FROM Schedule WHERE username "사용자명";
 
-UPDATE Schedule SET title = '수정 제목' WHERE username = "사용자명";
+UPDATE Schedule SET title = '수정 제목' WHERE scheduleId = 1;
 
-DELETE FROM Schedule WHERE username = "사용자명";
+DELETE FROM Schedule WHERE scheduleId = 1;
 
